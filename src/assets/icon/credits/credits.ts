e0821985a35df94110c69e96b192174eb5fd52ca
Domenico Posillipo <domposi@gmail.com>
2024-12-11 19:16:56 +0100

    Added icons and credits

---
+++ src/assets/icon/credits/credits.ts
@@ -0,0 +1,3 @@
+export const flatIcon: string[] = [
+    '<a href="https://www.flaticon.com/free-icons/surprise-box" title="surprise box icons">Surprise box icons created by kornkun - Flaticon</a>'
+]
