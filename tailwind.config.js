5257056e6022b06376a299c0dfc5ebeb9b270ab7
Domenico Posillipo <domposi@gmail.com>
2024-12-11 19:37:45 +0100

    Added "Cinzel" font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cinzel' : ['Cinzel', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
