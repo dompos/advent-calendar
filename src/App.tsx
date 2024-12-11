ac1f104da6a71cde7d4be6e25025385f72ae79bc
Domenico Posillipo <domposi@gmail.com>
2024-12-11 18:52:37 +0100

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { RouterProvider, createRoutesFromElements, createBrowserRouter, Route } from "react-router-dom";
+import MobileLayout from "./layout/MobileLayout";
+import HomePage from "./pages/HomePage";
+
 
 const App = () => {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MobileLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    )
+  );
+
   return (
-    <div className="text-5xl">App</div>
+    <RouterProvider router={router} />
   )
 }
 
