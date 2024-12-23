f9eac143ef69fcd0faed57a6ec35d91074cab746
Domenico Posillipo <domposi@gmail.com>
2024-12-23 17:42:06 +0100

    Added placeholder text

--- src/components/DayModal.tsx
+++ src/components/DayModal.tsx
@@ -16,8 +16,9 @@ const DayModal = ({ isOpen, onClose, dayNumber }: DayModalProps) => {
             setAnimate('animate-slideOutLeft animate-slow');
             setTimeout(onClose, 1100);
             }} 
-            className={`h-screen w-screen flex justify-center ${animate}`}>
-            <h1>{dayNumber}</h1>
+            className={`h-screen w-screen flex flex-col items-center gap-4 mt-8 ${animate}`}>
+            <h1 className="font-candy text-bagPink text-4xl">Day {dayNumber}</h1>
+            <p className="font-candy text-bagPink text-center mx-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est laboriosam ipsa, modi illum error voluptate repudiandae quo assumenda iusto, reiciendis voluptatibus qui esse quam! Deleniti qui veritatis harum officiis ea?</p>
             <img className="size-96" src={imgSrc} alt="Placeholder" />
         </div>
     )
